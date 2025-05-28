e1ea5d9734e243b64b284128b3877b51a291e3b2
king8879 <k4507244@gmail.com>
2025-05-28 13:05:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,14 +39,14 @@ THUMB_URL = os.environ.get("THUMB_URL", "https://envs.sh/9HT.jpg")
 
 
 # # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
+# API_ID = int(os.environ.get("API_ID", "27262405"))
+# API_HASH = os.environ.get("API_HASH", "33011215edbd8c0f6b710df2fdf0b088")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7799243917:AAHpsOOIKswmUA9XQ0lk2SMGePGV-WnVvuQ")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Extractorvicky_bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "6768714745"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6768714745").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002609761797"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002526911796"))
 # THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
 
